213bbedca8aee9dc9f6b16dfb72e031cc460bfc2
jinoung <jinoung@gmail.com>
2021-11-11 17:00:10 +0900

    config 변경

--- bbs/__init__.py
+++ bbs/__init__.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-import config
+#import config
 
 naming_convention = {
     "ix": 'ix_%(column_0_label)s',
@@ -17,7 +17,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(config)
+    app.config.from_object('APP_CONFIG_FILE')
 
     # ORM
     db.init_app(app)
